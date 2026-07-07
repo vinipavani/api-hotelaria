e6380a34b838058d7bd9655e65cc651cee9f2cc3
vinipavani <viniciusalvespavani@hotmail.com>
2026-07-07 19:18:23 -0300

    setup: create database connection method and close connection method

---
+++ internal/database/database.go
@@ -0,0 +1,56 @@
+package database
+
+import (
+	"context"
+	"log"
+	"os"
+	"time"
+	"github.com/jackc/pgx/v5/pgxpool"
+)
+
+func ConnectDB() {
+	databaseURL := getDatabaseURL()
+
+	config, err := pgxpool.ParseConfig(databaseURL)
+	if err != nil {
+		log.Fatalf("Erro ao processar a string de conexão do banco: %v\n", err)
+	}
+
+	config = configDatabaseConnection(config)
+
+	DB, err = pgxpool.NewWithConfig(context.Background(), config)
+	if err != nil {
+		log.Fatalf("Erro ao criar o pool de conexões do Postgres: %v\n", err)
+	}
+
+	err = DB.Ping(context.Background())
+	if err != nil {
+		log.Fatalf("O contêiner do Postgres respondeu, mas recusou a conexão: %v\n", err)
+	}
+
+	log.Println("Conexão com o PostgreSQL do Docker estabelecida com sucesso!")
+}
+
+func CloseDB() {
+	if DB != nil {
+		DB.Close()
+		log.Println("Pool de conexões com o banco de dados fechado com sucesso.")
+	}
+}
+
+func getDatabaseURL() string {
+	databaseURL := os.Getenv("DATABASE_URL")
+	if databaseURL == "" {
+		databaseURL = "postgres://postgres:postgres@db:5432/api_db?sslmode=disable"
+	}
+
+	return databaseURL
+}
+
+func configDatabaseConnection(config *pgxpool.Config) *pgxpool.Config {
+	config.MaxConns = 10
+	config.MinConns = 2
+	config.MaxConnIdleTime = time.Minute * 5 // Fecha conexões inativas após 5 minutos
+	
+	return config
+}
