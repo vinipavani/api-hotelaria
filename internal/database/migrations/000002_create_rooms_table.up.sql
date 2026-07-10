2e3ca98d9b7570923922eb9bf52f094054b216a7
vinipavani <viniciusalvespavani@hotmail.com>
2026-07-09 22:51:58 -0300

    feat: add create rooms table migration

---
+++ internal/database/migrations/000002_create_rooms_table.up.sql
@@ -0,0 +1,10 @@
+CREATE TABLE IF NOT EXISTS rooms (
+    id BIGSERIAL PRIMARY KEY,
+    hotel_id BIGINT NOT NULL REFERENCES hotels(id) ON DELETE CASCADE,
+    number VARCHAR(4) NOT NULL,
+    type VARCHAR(50) NOT NULL CHECK (type IN ('single', 'double', 'suite')),
+    per_night_value NUMERIC(10, 2) NOT NULL,
+    created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT unique_hotel_room_number UNIQUE (hotel_id, number)
+);
