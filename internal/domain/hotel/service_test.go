2201edaff0a968851a03a474deee08baa3874fe1
vinipavani <viniciusalvespavani@hotmail.com>
2026-07-13 15:01:10 -0300

    test: add test for create hotel service

---
+++ internal/domain/hotel/service_test.go
@@ -0,0 +1,39 @@
+package hotel
+
+import (
+	"context"
+	"testing"
+)
+
+type mockHotelRepository struct {
+	onFindAll func(ctx context.Context) ([]*Hotel, error)
+	onInsert  func(ctx context.Context, h *Hotel) error
+}
+
+func (m *mockHotelRepository) FindAll(ctx context.Context) ([]*Hotel, error) {
+	return m.onFindAll(ctx)
+}
+
+func (m *mockHotelRepository) Insert(ctx context.Context, h *Hotel) error {
+	return m.onInsert(ctx, h)
+}
+
+func TestCreateHotel_Suite(t *testing.T) {
+	ctx := context.Background()
+
+	t.Run("should create hotel successfully under ideal conditions", func(t *testing.T) {
+		hotelRepoMock := &mockHotelRepository{
+			onInsert: func(ctx context.Context, h *Hotel) error {
+				return nil
+			},
+		}
+
+		service := NewService(hotelRepoMock)
+		input := CreateHotelInput{Name: "Copacabana Palace", City: "Rio de Janeiro"}
+		_, err := service.CreateHotel(ctx, input)
+
+		if err != nil {
+			t.Fatalf("esperava erro nulo, recebeu: %v", err)
+		}
+	})
+}
